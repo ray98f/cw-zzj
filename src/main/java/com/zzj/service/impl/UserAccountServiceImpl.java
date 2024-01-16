608950b57d2f0e0b1917a70b8567fe11aea93199
Ray <fengruipeng>
2024-01-16 10:14:36 +0800

    fix:答题分数修改为百位

--- src/main/java/com/zzj/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/zzj/service/impl/UserAccountServiceImpl.java
@@ -190,7 +190,7 @@ public class UserAccountServiceImpl implements UserAccountService {
         er.setUserId(currentLoginUser.getUserId());
         er.setReCount(examList.length);
         er.setReCountError(errCount);
-        er.setRePercent((double) (((examList.length - errCount) * 100) / examList.length));
+        er.setRePercent((Math.round((float) ((examList.length - errCount) * 100) / examList.length) / 100.0) * 100);
 
         try {
             Integer result = dmUserAccountMapper.addRecord(er);
